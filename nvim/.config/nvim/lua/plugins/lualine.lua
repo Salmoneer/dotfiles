9a4a5dc7b20b536e3f8da1a1353777a5b05f2dd9
Salmoneer <camogisan@gmail.com>
2024-04-27 02:58:16 +1000

    feat(tmux,nvim): merge neovim and tmux status lines into one; redo tmux status line

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,11 +1,14 @@
 return {
     "nvim-lualine/lualine.nvim",
+    dependencies = {
+        "vimpostor/vim-tpipeline"
+    },
     opts = {
         options = {
             icons_enabled = false,
-            theme = 'auto',
-            component_separators = { left = '', right = ''},
-            section_separators = { left = '', right = ''},
+            theme = "auto",
+            component_separators = { left = "", right = "" },
+            section_separators   = { left = "", right = "" },
             disabled_filetypes = {
               statusline = {},
               winbar = {},
@@ -14,16 +17,17 @@ return {
             always_divide_middle = true,
             globalstatus = false,
             refresh = {
-              statusline = 1000,
-              tabline = 1000,
-              winbar = 1000,
+              statusline = 10,
+              tabline = 10,
+              winbar = 10,
             }
         },
+
         sections = {
-            lualine_a = {'mode'},
-            lualine_b = {'branch', 'diff',
+            lualine_a = {"mode"},
+            lualine_b = {"branch", "diff",
                 {
-                    'diagnostics',
+                    "diagnostics",
                     symbols = {
                         error = " ",
                         warn  = " ",
@@ -32,22 +36,10 @@ return {
                     }
                 },
             },
-            lualine_c = {'filename'},
+            lualine_c = {"filename"},
             lualine_x = {'encoding', 'fileformat', 'filetype'},
-            lualine_y = {'progress'},
-            lualine_z = {'location'}
+            lualine_y = {"progress"},
+            lualine_z = {"location"}
         },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = {'filename'},
-            lualine_x = {'location'},
-            lualine_y = {},
-            lualine_z = {}
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {}
     },
 }
