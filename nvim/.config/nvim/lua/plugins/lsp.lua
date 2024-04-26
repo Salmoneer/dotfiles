964299d19aa71140279b433746eb05452ee7b90e
Salmoneer <camogisan@gmail.com>
2024-04-26 21:22:34 +1000

    fix: Remove undefined global vim error

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -16,9 +16,21 @@ return {
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
             require("mason-lspconfig").setup_handlers({
                 function(server_name)
-                    require("lspconfig")[server_name].setup({
-                        capabilities = capabilities
-                    })
+                    if server_name == "lua_ls" then
+                        require("lspconfig").lua_ls.setup({
+                            settings = {
+                                Lua = {
+                                    diagnostics = {
+                                        globals = { "vim" }
+                                    }
+                                }
+                            }
+                        })
+                    else
+                        require("lspconfig")[server_name].setup({
+                            capabilities = capabilities
+                        })
+                    end
                 end
             })
         end
