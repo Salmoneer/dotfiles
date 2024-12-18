7ae32cd7b2fb8bfdf3bfad4e63dbda68d9a1afe9
Salmoneer <camogisan@gmail.com>
2024-12-18 14:19:58 +1100

    feat(nvim): add variety of colorschemes, change back to catppuccin macchiato

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,23 @@
 return {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-        vim.o.background = "dark"
-        vim.cmd.colorscheme("gruvbox")
-    end,
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        opts = {},
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        opts = {},
+    },
+    {
+        "folke/tokyonight.nvim",
+        priority = 1000,
+        opts = {},
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        priority = 1000,
+        opts = {},
+    },
 }
