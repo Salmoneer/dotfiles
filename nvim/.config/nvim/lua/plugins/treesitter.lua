619382d3daf88e19f6d0291617d8a477d320c358
Salmoneer <camogisan@gmail.com>
2024-12-18 14:18:36 +1100

    feat(nvim): make treesitter highlight enabled by default, and install filetypes on buffer open

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,13 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            auto_install = true,
+            highlight = {
+              enable = true,
+              disable = {},
+            },
+        })
+    end
 }
